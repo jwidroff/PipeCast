10dec3b8fdb4349c6107df237726977cbc38c9a2
Jeffery Widroff <jeff.widroff@gmail.com>
2020-08-17 08:00:44 -0400

    Added class to previous commit that was left out

---
+++ MultiColorCrush/GridPoints.swift
@@ -0,0 +1,112 @@
+//
+//  GridPoints.swift
+//  MultiColorCrush
+//
+//  Created by Jeffery Widroff on 8/10/20.
+//  Copyright © 2020 Jeffery Widroff. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class GridPoints {
+    
+    var frame = CGRect()
+    var height = CGFloat()
+    var width = CGFloat()
+    var xIndex = Int()
+    var yIndex = Int()
+    var dictionary = [Indexes: CGPoint]()
+    var heightPoints = [CGFloat]()
+    var widthPoints = [CGFloat]()
+
+    init(frame: CGRect, height: Int, width: Int) {
+        
+
+        self.height = CGFloat(height)
+        self.width = CGFloat(width)
+        self.frame = frame
+    }
+    
+    func getGrid() -> [Indexes: CGPoint]{
+        
+        let heightDistance = frame.height / height
+        let widthDistance = frame.width / width
+        var point:CGFloat = heightDistance / 2
+
+        //Get the CGFloat for each height and add to array
+        for _ in 0..<Int(height) {
+            
+            heightPoints.append(point)
+            point += heightDistance
+        }
+        
+        
+        //Get the CGFloat for each width and add to array
+        point = widthDistance / 2
+        
+        for _ in 0..<Int(width) {
+            
+            widthPoints.append(point)
+            point += widthDistance
+        }
+        
+        //Create temp arrays of (Int, CGFloat) for height to be later used to combine with width
+        var counter = 0
+        var heightArray = [(Int, CGFloat)]()
+        for point in heightPoints {
+
+            let pair:(Int, CGFloat) = (counter, point)
+            heightArray.append(pair)
+            counter += 1
+        }
+                
+        //Create temp arrays of (Int, CGFloat) for width to be later used to combine with height
+        counter = 0
+        var widthArray = [(Int, CGFloat)]()
+        for point in widthPoints {
+
+            let pair:(Int, CGFloat) = (counter, point)
+            widthArray.append(pair)
+            counter += 1
+        }
+        
+        for verticalSet in heightArray {
+            
+            for horizontalSet in widthArray {
+                
+                let index = Indexes(x: verticalSet.0, y: horizontalSet.0)
+                let floatingPoint = CGPoint(x: horizontalSet.1, y: verticalSet.1)
+                
+                print("index \(index)")
+                print("floatingPoint \(floatingPoint)")
+
+                dictionary[Indexes(x: verticalSet.0, y: horizontalSet.0)] = floatingPoint
+            }
+        }
+        return dictionary
+    }
+}
+
+struct Indexes {
+    var x: Int
+    var y: Int
+}
+
+extension Indexes: Hashable {
+    
+    static func == (lhs: Indexes, rhs: Indexes) -> Bool {
+        return lhs.x == rhs.x && lhs.y == rhs.y
+    }
+
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(x)
+        hasher.combine(y)
+    }
+}
+
+
+
+
+
+
