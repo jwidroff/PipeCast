b2fb900c0ef29b228990f8a5031e3c658a1e6df4
Jeffery Widroff <jeff.widroff@gmail.com>
2020-10-20 09:18:42 -0400

    Made it that the pieces color in the ShapeView are now the colors coming from the parameters in the func call

--- MultiColorCrush/ShapeView.swift
+++ MultiColorCrush/ShapeView.swift
@@ -88,156 +88,6 @@ class ShapeView : UIView {
         
         switch name {
             
-//        case "square":
-//            guard let context = UIGraphicsGetCurrentContext() else { return }
-//            let width = frame.width / 4 * 2
-//            let height = frame.height / 4 * 2
-//            let x =  -(width - frame.width) / 2
-//            let y =  -(height - frame.height) / 2
-//            let corner1 = CGPoint(x: rect.minX + (frame.width / 10), y: rect.minY + (frame.width / 10))
-//            let corner2 = CGPoint(x: rect.minX + (frame.width / 10), y: rect.maxY - (frame.width / 10))
-//            let corner3 = CGPoint(x: rect.maxX - (frame.width / 10), y: rect.maxY - (frame.width / 10))
-//            let corner4 = CGPoint(x: rect.maxX - (frame.width / 10), y: rect.minY + (frame.width / 10))
-//            let holeRect = CGRect(x: x, y: y, width: width, height: height)
-//            context.beginPath()
-//            context.move(to: corner1)
-//            context.addLine(to: corner2)
-//            context.addLine(to: corner3)
-//            context.addLine(to: corner4)
-//            context.closePath()
-//            context.setFillColor(color!)
-//            context.fillPath()
-//            context.clear(holeRect)
-            
-//        case "star":
-//            guard let context = UIGraphicsGetCurrentContext() else { return }
-//
-//            let rectSideLength = rect.width
-//            let rectSidePortion = rectSideLength / 5
-//            let cushion = rectSideLength / 4
-//            let corner1 = CGPoint(x: rect.minX, y: rectSidePortion * 2)
-//            let corner2 = CGPoint(x: rectSidePortion * 1.5, y: (rectSidePortion * 2))
-//            let corner3 = CGPoint(x: rectSidePortion * 2.5, y: rect.minY)
-//            let corner4 = CGPoint(x: rect.maxX - (rectSidePortion * 1.5), y: (rectSidePortion * 2))
-//            let corner5 = CGPoint(x: rect.maxX, y: rectSidePortion * 2)
-//            let corner6 = CGPoint(x: rect.maxX - cushion, y: rectSidePortion * 3)
-//            let corner7 = CGPoint(x: rectSidePortion * 4.5, y: rect.maxY)
-//            let corner8 = CGPoint(x: rectSidePortion * 2.5, y: rect.maxY - cushion)
-//            let corner9 = CGPoint(x: rectSidePortion * 0.5, y: rect.maxY)
-//            let corner10 = CGPoint(x: rect.minX + cushion, y: rectSidePortion * 3)
-//
-//            context.beginPath()
-//            context.move(to: corner1)
-//            context.addLine(to: corner2)
-//            context.addLine(to: corner3)
-//            context.addLine(to: corner4)
-//            context.addLine(to: corner5)
-//            context.addLine(to: corner6)
-//            context.addLine(to: corner7)
-//            context.addLine(to: corner8)
-//            context.addLine(to: corner9)
-//            context.addLine(to: corner10)
-//            context.closePath()
-//            context.setFillColor(color!)
-//            context.fillPath()
-            
-//        case "triangle":
-//
-//            guard let context = UIGraphicsGetCurrentContext() else { return }
-//            let width = frame.width / 4 * 1
-//            let height = frame.height / 4 * 1
-//            let x =  -(width - frame.width) / 2
-//            let y =  -(height - frame.height) / 1.5
-//            let corner1 = CGPoint(x: rect.minX, y: rect.maxY)
-//            let corner2 = CGPoint(x: rect.maxX, y: rect.maxY)
-//            let corner3 = CGPoint(x: rect.midX, y: rect.minY)
-//            let holeRect = CGRect(x: x, y: y, width: width, height: height)
-//
-//            context.beginPath()
-//            context.move(to: corner1)
-//            context.addLine(to: corner2)
-//            context.addLine(to: corner3)
-//            context.closePath()
-//            context.setFillColor(color!)
-//            context.fillPath()
-//            context.clear(holeRect)
-            
-//        case "cross":
-//            guard let context = UIGraphicsGetCurrentContext() else { return }
-//            let width = frame.width / 4 * 0.75
-//            let height = frame.height / 4 * 2
-//            let x =  -(width - frame.width) / 2
-//            let y =  -(height - frame.height) / 2
-//            let holeRect = CGRect(x: x, y: y, width: width, height: height)
-//
-//            let width2 = frame.width / 4 * 2
-//            let height2 = frame.height / 4 * 0.75
-//            let x2 =  -(width2 - frame.width) / 2
-//            let y2 =  -(height2 - frame.height) / 2
-//            let holeRect2 = CGRect(x: x2, y: y2, width: width2, height: height2)
-//
-//            let pos1 = CGPoint(x: rect.width / 3 * 2, y: rect.minY)
-//            let pos2 = CGPoint(x: rect.width / 3 * 1, y: rect.minY)
-//            let pos3 = CGPoint(x: rect.width / 3 * 1, y: rect.height / 3 * 1)
-//            let pos4 = CGPoint(x: rect.minX, y: rect.height / 3 * 1)
-//            let pos5 = CGPoint(x: rect.minX, y: rect.height / 3 * 2)
-//            let pos6 = CGPoint(x: rect.width / 3 * 1, y: rect.height / 3 * 2)
-//            let pos7 = CGPoint(x: rect.width / 3 * 1, y: rect.maxY)
-//            let pos8 = CGPoint(x: rect.width / 3 * 2, y: rect.maxY)
-//            let pos9 = CGPoint(x: rect.width / 3 * 2, y: rect.height / 3 * 2)
-//            let pos10 = CGPoint(x: rect.maxX, y: rect.height / 3 * 2)
-//            let pos11 = CGPoint(x: rect.maxX, y: rect.height / 3 * 1)
-//            let pos12 = CGPoint(x: rect.width / 3 * 2, y: rect.height / 3 * 1)
-//
-//            context.beginPath()
-//            context.move(to: pos1)
-//            context.addLine(to: pos2)
-//            context.addLine(to: pos3)
-//            context.addLine(to: pos4)
-//            context.addLine(to: pos5)
-//            context.addLine(to: pos6)
-//            context.addLine(to: pos7)
-//            context.addLine(to: pos8)
-//            context.addLine(to: pos9)
-//            context.addLine(to: pos10)
-//            context.addLine(to: pos11)
-//            context.addLine(to: pos12)
-//            context.closePath()
-//            context.setFillColor(color!)
-//            context.fillPath()
-//            context.clear(holeRect)
-//            context.clear(holeRect2)
-            
-//        case "octigon":
-//            guard let context = UIGraphicsGetCurrentContext() else { return }
-//            let width = frame.width / 4 * 1
-//            let height = frame.height / 4 * 1
-//            let x =  -(width - frame.width) / 2
-//            let y =  -(height - frame.height) / 2
-//            let holeRect = CGRect(x: x, y: y, width: width, height: height)
-//            let leftTop = CGPoint(x: rect.width / 3 * 1, y: rect.minY)
-//            let rightTop = CGPoint(x: rect.width / 3 * 2, y: rect.minY)
-//            let topLeft =  CGPoint(x: rect.minX, y: rect.height / 3 * 1)
-//            let bottomLeft =  CGPoint(x: rect.minX, y: rect.height / 3 * 2)
-//            let topRight =  CGPoint(x: rect.maxX, y: rect.height / 3 * 1)
-//            let bottomright =  CGPoint(x: rect.maxX, y: rect.height / 3 * 2)
-//            let leftBottom = CGPoint(x: rect.width / 3 * 1, y: rect.maxY)
-//            let rightBottom = CGPoint(x: rect.width / 3 * 2, y: rect.maxY)
-//
-//            context.beginPath()
-//            context.move(to: rightTop)
-//            context.addLine(to: leftTop)
-//            context.addLine(to: topLeft)
-//            context.addLine(to: bottomLeft)
-//            context.addLine(to: leftBottom)
-//            context.addLine(to: rightBottom)
-//            context.addLine(to: bottomright)
-//            context.addLine(to: topRight)
-//            context.closePath()
-//            context.setFillColor(color!)
-//            context.fillPath()
-//            context.clear(holeRect)
-            
         case .regular:
             
             guard let context = UIGraphicsGetCurrentContext() else { return }
@@ -274,7 +124,7 @@ class ShapeView : UIView {
                     
                     let rect1 = CGRect(x: (-frame.width / 2), y: (-frame.height / 2), width: posWidth, height: posHeight)
                     context.addEllipse(in: rect1)
-                    context.setFillColor(UIColor.blue.cgColor)
+                    context.setFillColor(color!)
                     context.fillEllipse(in: rect1)
                     
                     let rect2 = CGRect(x: (-frame.width / 2), y: (-frame.height / 2), width: negWidth, height: negHeight)
@@ -288,7 +138,7 @@ class ShapeView : UIView {
                     
                         let rect3 = CGRect(x: (frame.width / 2) - (diff), y: (-frame.height / 2), width: posWidth, height: posHeight)
                         context.addEllipse(in: rect3)
-                        context.setFillColor(UIColor.blue.cgColor)
+                        context.setFillColor(color!)
                         context.fillEllipse(in: rect3)
                         
                         let rect4 = CGRect(x: (frame.width / 2) + (diff), y: (-frame.height / 2), width: negWidth, height: negHeight)
@@ -308,7 +158,7 @@ class ShapeView : UIView {
 
                         let rect5 = CGRect(x: (-frame.width / 2), y: (frame.height / 2) - diff, width: posWidth, height: posHeight)
                         context.addEllipse(in: rect5)
-                        context.setFillColor(UIColor.green.cgColor)
+                        context.setFillColor(color!)
                         context.fillEllipse(in: rect5)
                         
                         let rect6 = CGRect(x: (-frame.width / 2), y: (frame.height / 2) + diff , width: negWidth, height: negHeight)
@@ -324,7 +174,7 @@ class ShapeView : UIView {
                         
                         let rect1 = CGRect(x: (-frame.width / 2), y: (-frame.height / 2), width: posWidth, height: posHeight)
                         context.addEllipse(in: rect1)
-                        context.setFillColor(UIColor.green.cgColor)
+                        context.setFillColor(color!)
                         context.fillEllipse(in: rect1)
                         
                         let rect2 = CGRect(x: (-frame.width / 2), y: (-frame.height / 2), width: negWidth, height: negHeight)
@@ -344,7 +194,7 @@ class ShapeView : UIView {
 
                         let rect7 = CGRect(x: (frame.width / 2) - (diff), y: (frame.height / 2) - diff, width: posWidth, height: posHeight)
                         context.addEllipse(in: rect7)
-                        context.setFillColor(UIColor.yellow.cgColor)
+                        context.setFillColor(color!)
                         context.fillEllipse(in: rect7)
                         
                         let rect8 = CGRect(x: (frame.width / 2) + (diff), y: (frame.height / 2) + diff, width: negWidth, height: negHeight)
@@ -360,7 +210,7 @@ class ShapeView : UIView {
                         
                         let rect5 = CGRect(x: (-frame.width / 2), y: (frame.height / 2) - diff, width: posWidth, height: posHeight)
                         context.addEllipse(in: rect5)
-                        context.setFillColor(UIColor.yellow.cgColor)
+                        context.setFillColor(color!)
                         context.fillEllipse(in: rect5)
                         
                         let rect6 = CGRect(x: (-frame.width / 2), y: (frame.height / 2) + diff , width: negWidth, height: negHeight)
@@ -378,7 +228,7 @@ class ShapeView : UIView {
                         
                         let rect3 = CGRect(x: (frame.width / 2) - (diff), y: (-frame.height / 2), width: posWidth, height: posHeight)
                         context.addEllipse(in: rect3)
-                        context.setFillColor(UIColor.white.cgColor)
+                        context.setFillColor(color!)
                         context.fillEllipse(in: rect3)
                         
                         let rect4 = CGRect(x: (frame.width / 2) + (diff), y: (-frame.height / 2), width: negWidth, height: negHeight)
@@ -396,7 +246,7 @@ class ShapeView : UIView {
                         
                         let rect7 = CGRect(x: (frame.width / 2) - (diff), y: (frame.height / 2) - diff, width: posWidth, height: posHeight)
                         context.addEllipse(in: rect7)
-                        context.setFillColor(UIColor.white.cgColor)
+                        context.setFillColor(color!)
                         context.fillEllipse(in: rect7)
                         
                         let rect8 = CGRect(x: (frame.width / 2) + (diff), y: (frame.height / 2) + diff, width: negWidth, height: negHeight)
@@ -408,46 +258,11 @@ class ShapeView : UIView {
                         
                     }
                     
-                    
-                    
-                    
-                    
                 default:
                     break
                 
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
                 }
                 
-                
-                
-                
-           
-                
-             
-            
-            
-            
-//            let eclipseHeight3 = frame.height
-//            let eclipseWidth3 = frame.width
-//            let rect3 = CGRect(x: (-frame.width / 2), y: (-frame.height / 2), width: eclipseWidth3, height: eclipseHeight3)
-//            context.addEllipse(in: rect3)
-//            context.setFillColor(UIColor.brown.cgColor)
-//            context.fillEllipse(in: rect3)
-            
-            
-            
-            
-            
             
         case .doubleElbow:
             
